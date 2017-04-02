3e4ff0915d0318cfe9df1e6512b72f5a9640be42
hummel22 <hummel.kevin@gmail.com>
2017-04-02 13:40:15 -0500

    t0000 - Add port to be deinfed by enviroment

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8081
 
 app.get('/', function (req, res) {
      res.send('Hello World');
@@ -51,9 +56,8 @@ app.get('/apidocs', function (req, res) {
 })    
     
 
-var server = app.listen(8081, function () {
-     var host = server.address().address
-          var port = server.address().port
-             
-             console.log("Example app listening at http://%s:%s", host, port)
+var server = app.listen(port, function () {
+     var host = server.address().address;
+     var port = server.address().port;
+     console.log("Example app listening at http://%s:%s", host, port);
 })
