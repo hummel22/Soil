27fc7e5f9356b8a5a29025db7776e40d2be32c35
hummel22 <hummel.kevin@gmail.com>
2017-04-19 23:25:37 -0400

    t0026 - Connection Made to local hosted MongoDb

--- server.js
+++ server.js
@@ -9,43 +9,12 @@ var port = process.env.PORT || 8081
 require('./routes')(app)
 
 
+var MongoClient = require('mongodb').MongoClient;
 
-/**
- *
- * Route structure  - 
- *  Api Routes are located in /api/-/index.js
- *
- *  View Routes are listed in routes.js
- *    TODO view directorytree structure
- */
-
-/**
- * @api {get} /user/:id Request User information
- * @apiName GetUser
- * @apiGroup User
- *
- * @apiParam {Number} id Users unique ID.
- *
- * @apiSuccess {String} firstname Firstname of the User.
- * @apiSuccess {String} lastname  Lastname of the User.
- *
- * @apiSuccessExample Success-Response:
- *     HTTP/1.1 200 OK
- *     {
- *       "firstname": "John",
- *       "lastname": "Doe"
- *     }
- *
- * @apiError UserNotFound The id of the User was not found.
- *
- * @apiErrorExample Error-Response:
- *     HTTP/1.1 404 Not Found
- *     {
- *       "error": "UserNotFound"
- *     }
- */
-
-
+// Connect to the db
+MongoClient.connect("mongodb://localhost:27017/Soil", function (err, db) {
+ if(err) throw err;
+});
 
 var server = app.listen(port, function () {
      var host = server.address().address;
